0de01a9eabacb29dfa796ea00f65700adc7c294b
it21111860 <87365647+Navodhyait2111860@users.noreply.github.com>
2021-07-17 13:17:18 +0000

    completed the tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,20 @@
 
 int main() {
   
+  int x, y;
+  int sum, average;
+
+  printf("enter the number ");
+  scanf("%d",&x);
+  printf("enter the number ");
+  scanf("%d", &y);
+
+  sum = x + y;
+  printf("sum of the numbers %d\n",sum);
+
+ average = sum /2;
+ printf("average of the numbers %d", average);
+  
   return 0;
 }
 
